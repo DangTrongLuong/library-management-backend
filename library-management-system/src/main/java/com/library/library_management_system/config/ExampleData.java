b72c27b3791ccb3d048f4c0fcd90eaf11aabb9c8
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-10-21 10:12:11 +0700

    Them du lieu vao bang Shift

--- library-management-system/src/main/java/com/library/library_management_system/config/ExampleData.java
+++ library-management-system/src/main/java/com/library/library_management_system/config/ExampleData.java
@@ -1,11 +1,15 @@
 package com.library.library_management_system.config;
 
+import java.math.BigDecimal;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.library.library_management_system.entity.Category;
+import com.library.library_management_system.entity.Librarian.Shift;
 import com.library.library_management_system.repository.CategoryRepository;
+import com.library.library_management_system.repository.Librarian.ShiftRepository;
 
 @Configuration
 public class ExampleData {
@@ -20,6 +24,19 @@ public class ExampleData {
                 categoryRepository.save(new Category(null, "Phiêu lưu", "Các câu chuyện khám phá, mạo hiểm", "D4", "Dữ liệu mẫu"));
                 categoryRepository.save(new Category(null, "Tâm lý", "Truyện phân tích tâm lý con người", "E5", "Dữ liệu mẫu"));
 
+                System.out.println("Dữ liệu mẫu thể loại đã được thêm vào cơ sở dữ liệu.");
+            }
+        };
+    }
+
+    @Bean
+    CommandLineRunner initDatabaseShift(ShiftRepository shiftRepository) {
+        return args -> {
+            if (shiftRepository.count() == 0) {
+                shiftRepository.save(new Shift(null, "Morning", "8h-12h", BigDecimal.valueOf(4.0) ));
+                shiftRepository.save(new Shift(null, "Afternoon", "13h-17h", BigDecimal.valueOf(4.0) ));
+                shiftRepository.save(new Shift(null, "Evening", "19h-23h", BigDecimal.valueOf(4.0) ));
+                shiftRepository.save(new Shift(null, "Fulltime", "8h-17h", BigDecimal.valueOf(8.0) ));
                 System.out.println("Dữ liệu mẫu thể loại đã được thêm vào cơ sở dữ liệu.");
             }
         };
