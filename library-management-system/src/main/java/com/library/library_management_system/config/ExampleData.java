fcaea3404b1df6ce06a5190c007bf74349b1b27a
Lilidelion <nguyenminh569954@gmail.com>
2025-10-21 15:59:23 +0700

    Cap nhật dữ liệu
    Thêm dữ liệu

--- library-management-system/src/main/java/com/library/library_management_system/config/ExampleData.java
+++ library-management-system/src/main/java/com/library/library_management_system/config/ExampleData.java
@@ -19,10 +19,13 @@ public class ExampleData {
         return args -> {
             if (categoryRepository.count() == 0) {
                 categoryRepository.save(new Category(null, "Khoa học viễn tưởng", "Thể loại về khoa học và tương lai", "A1", "Dữ liệu mẫu"));
-                categoryRepository.save(new Category(null, "Lãng mạn", "Chuyện tình yêu", "B2", "Dữ liệu mẫu"));
-                categoryRepository.save(new Category(null, "Kinh dị", "Truyện ma, rùng rợn", "C3", "Dữ liệu mẫu"));
-                categoryRepository.save(new Category(null, "Phiêu lưu", "Các câu chuyện khám phá, mạo hiểm", "D4", "Dữ liệu mẫu"));
-                categoryRepository.save(new Category(null, "Tâm lý", "Truyện phân tích tâm lý con người", "E5", "Dữ liệu mẫu"));
+                categoryRepository.save(new Category(null, "Lãng mạn", "Chuyện tình yêu", "B1", "Dữ liệu mẫu"));
+                categoryRepository.save(new Category(null, "Kinh dị", "Truyện ma, rùng rợn", "C1", "Dữ liệu mẫu"));
+                categoryRepository.save(new Category(null, "Phiêu lưu", "Các câu chuyện khám phá, mạo hiểm", "D1", "Dữ liệu mẫu"));
+                categoryRepository.save(new Category(null, "Tâm lý", "Truyện phân tích tâm lý con người", "E1", "Dữ liệu mẫu"));
+                categoryRepository.save(new Category(null, "Manga", "Truyện tranh Nhật Bản", "A3", "Truyên tranh Nhật Bản"));
+                categoryRepository.save(new Category(null, "Light Novel", "Tiêu thuyết Nhật Bản", "E10", "Dành cho thanh thiếu niên 20 đổ lên"));
+                categoryRepository.save(new Category(null, "Yandere", "Độc chiếm tình cảm ", "D4", "Dữ liệu thêm"));
 
                 System.out.println("Dữ liệu mẫu thể loại đã được thêm vào cơ sở dữ liệu.");
             }
