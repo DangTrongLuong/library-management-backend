04de8a3c354e60c94be8cf04647f9619daf750ff
DangTrongLuong <dtluong@cmcglobal.vn>
2025-10-22 16:34:32 +0700

    chinh sua api cho reader

--- library-management-system/src/main/java/com/library/library_management_system/enums/CardType.java
+++ library-management-system/src/main/java/com/library/library_management_system/enums/CardType.java
@@ -1,17 +1,7 @@
 package com.library.library_management_system.enums;
 
 public enum CardType {
-    BASIC("Bronze"),
-    PREMIUM("Silver"),
-    VIP("VIP");
-
-    private final String displayName;
-
-    CardType(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
+    BRONZE,
+    SILVER,
+    VIP
 }
