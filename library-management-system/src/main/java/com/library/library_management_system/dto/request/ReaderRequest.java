b87549214bab7f517e790aca3a1fa556850c052c
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-10-22 23:29:06 +0700

    fix api reader lan 2

--- library-management-system/src/main/java/com/library/library_management_system/dto/request/ReaderRequest.java
+++ library-management-system/src/main/java/com/library/library_management_system/dto/request/ReaderRequest.java
@@ -1,7 +1,19 @@
 package com.library.library_management_system.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
+
+import com.library.library_management_system.enums.CardType;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @NoArgsConstructor
