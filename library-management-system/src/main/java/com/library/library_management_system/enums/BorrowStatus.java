8542571eb38e6e5c17a1e5f2f864e35d34dab0b5
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-10-24 00:27:14 +0700

    Them file Enum va exception cho borrow

--- library-management-system/src/main/java/com/library/library_management_system/enums/BorrowStatus.java
+++ library-management-system/src/main/java/com/library/library_management_system/enums/BorrowStatus.java
@@ -1,5 +1,8 @@
 package com.library.library_management_system.enums;
 
 public enum BorrowStatus {
-    
+    BORROWED,
+    RETURNED,
+    OVERDUE,
+    AVAILABLE;
 }
--- library-management-system/src/main/java/com/library/library_management_system/exception/InsufficientStockException.java
+++ library-management-system/src/main/java/com/library/library_management_system/exception/InsufficientStockException.java
@@ -1,5 +1,11 @@
 package com.library.library_management_system.exception;
 
 public class InsufficientStockException extends RuntimeException {
-   
-}
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+
+    public InsufficientStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}   
