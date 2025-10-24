04d20567c361ddf49911f8728a0e2e7b4beac3b6
ibatg <nguyenhoanghuy328@gmail.com>
2025-10-24 15:25:08 +0700

    Thêm mapper, request, response cho Report

---
+++ library-management-system/src/main/java/com/library/library_management_system/dto/response/ReportResponse.java
@@ -0,0 +1,4 @@
+package com.library.library_management_system.dto.response;
+
+public class ReportResponse {
+}
---
+++ library-management-system/src/main/java/com/library/library_management_system/mapper/ReportMapper.java
@@ -0,0 +1,4 @@
+package com.library.library_management_system.mapper;
+
+public class ReportMapper {
+}
