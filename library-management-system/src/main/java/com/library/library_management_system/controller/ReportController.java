12b45fdb3bc3356b40a67ce79a51af7e61fc9aeb
ibatg <nguyenhoanghuy328@gmail.com>
2025-10-25 15:33:59 +0700

    Hàm lấy danh sách báo cáo

--- library-management-system/src/main/java/com/library/library_management_system/controller/ReportController.java
+++ library-management-system/src/main/java/com/library/library_management_system/controller/ReportController.java
@@ -1,4 +1,39 @@
 package com.library.library_management_system.controller;
 
+import com.library.library_management_system.dto.response.ReportResponse;
+import com.library.library_management_system.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/reports")
 public class ReportController {
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    // Hàm lấy tất cả báo cáo
+    @GetMapping("/getAllReport")
+    public ResponseEntity<List<ReportResponse>> getAllReports() {
+        List<ReportResponse> list = reportService.getAllReports();
+        if (list == null || list.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+    // Hàm lấy báo cáo theo ID
+    @GetMapping("/getReport/{id}")
+    public ResponseEntity<ReportResponse> getReportById(@PathVariable("id") String id) {
+        ReportResponse resp = reportService.getReportById(id);
+        if (resp == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(resp, HttpStatus.OK);
+    }
 }
