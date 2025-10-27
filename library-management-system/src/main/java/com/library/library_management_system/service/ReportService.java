7e9fa63e857c9607def2b14f41b5aae3c9b6e8f9
nguyenhongnhung1 <hiennguyen2270@gmail.com>
2025-10-27 16:41:49 +0700

    bao cao

--- library-management-system/src/main/java/com/library/library_management_system/service/ReportService.java
+++ library-management-system/src/main/java/com/library/library_management_system/service/ReportService.java
@@ -59,7 +59,7 @@ public class ReportService {
                 .collect(Collectors.toList());
     }
 
-    //ThemBbc
+
     @Transactional
     public ReportResponse createReport(ReportRequest request) {
         if (request.getReportId() != null && reportRepository.existsById(request.getReportId())) {
